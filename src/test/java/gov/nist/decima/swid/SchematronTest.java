93b73d3432caed7b5e51ce5ed65eca1ecdd1670e
david-waltermire-nist <david.waltermire@nist.gov>
2015-10-22 21:21:47 +0000

    Updated based on decima refactoring.

--- src/test/java/gov/nist/decima/swid/SchematronTest.java
+++ src/test/java/gov/nist/decima/swid/SchematronTest.java
@@ -17,45 +17,44 @@ import gov.nist.decima.assessment.result.AssessmentResults;
 import gov.nist.decima.assessment.result.BaseRequirementResult;
 import gov.nist.decima.assessment.result.DerivedRequirementResult;
 import gov.nist.decima.assessment.result.TestResult;
-import gov.nist.decima.assessment.schematron.DefaultSchematronEvaluator;
 import gov.nist.decima.assessment.schematron.SchematronAssessment;
-import gov.nist.decima.assessment.schematron.SchematronEvaluator;
-import gov.nist.decima.document.JDOMDocument;
+import gov.nist.decima.document.DefaultXMLDocumentFactory;
 import gov.nist.decima.document.XMLDocument;
 import gov.nist.decima.document.XMLDocumentException;
 import gov.nist.decima.requirement.RequirementsManager;
 import gov.nist.decima.schematron.DefaultSchematronCompiler;
 import gov.nist.decima.schematron.Schematron;
 import gov.nist.decima.schematron.SchematronCompilationException;
-import gov.nist.decima.schematron.SchematronCompiler;
 import gov.nist.decima.testing.StubRequirementsManager;
 
 public class SchematronTest {
 
 	@Test
 	public void temporaryTest() throws SchematronCompilationException, MalformedURLException, IOException, AssessmentException, XMLDocumentException {
-		// TODO: replace this once the requirements manager is implemented
-		SchematronCompiler schematronCompiler = new DefaultSchematronCompiler();
-		Schematron schematron = schematronCompiler.newSchematron(new URL("classpath:schematron/swid-nistir-8060.sch"));
-		SchematronEvaluator schematronEvaluator = new DefaultSchematronEvaluator(schematron);
+		// Load the document to assess
+		DefaultXMLDocumentFactory documentFactory = new DefaultXMLDocumentFactory();
+		XMLDocument doc = documentFactory.load(new URL("classpath:templates/primary-auth-swid.xml"));
+
+		// Load the schematron
+		Schematron schematron =  new DefaultSchematronCompiler().newSchematron(new URL("classpath:schematron/swid-nistir-8060.sch"));
+
+		// Create the assessment
+		SchematronAssessment assessment = new SchematronAssessment(schematron, "swid.primary.auth");
 		File resultDir = new File("svrl-result");
-		SchematronAssessment assessment = new SchematronAssessment(schematronEvaluator);
 		assessment.setResultDirectory(resultDir);
-		XMLDocument doc = new JDOMDocument(new URL("classpath:templates/primary-swid.xml"));
+//		resultDir.mkdirs();
+
+		// Perform the assessment
 		AssessmentResultBuilder builder = new AssessmentResultBuilder();
-		resultDir.mkdirs();
 		assessment.execute(doc, builder);
 		builder.end();
-		
-//
-//		ResultGeneratingSVRLHandler svrlHandler = new ResultGeneratingSVRLHandler(builder, doc);
-//		@SuppressWarnings("unused")
-//		SchematronOutputDocument output = SVRLParser.parse(svrlHandler, domResult.getNode());
 
-
+		// Generate the assessment results
+		// TODO: replace this once the requirements manager is implemented
 		RequirementsManager requirementsManager = new StubRequirementsManager(builder.getTestedDerivedRequirements());
 		AssessmentResults validationResult = builder.build(requirementsManager);
 
+		// Output the results
 		Collection<BaseRequirementResult> results = validationResult.getBaseRequirementResults();
 		for (BaseRequirementResult reqResult : results) {
 			System.out.println(reqResult.getBaseRequirement().getId() + ": status=" + reqResult.getStatus());
@@ -71,17 +70,21 @@ public class SchematronTest {
 	@Test
 	@Ignore
 	public void finalTest() throws SchematronCompilationException, MalformedURLException, IOException, AssessmentException, XMLDocumentException {
-		SchematronCompiler schematronCompiler = new DefaultSchematronCompiler();
-		Schematron schematron = schematronCompiler.newSchematron(new URL("classpath:schematron/swid-nistir-8060.sch"));
-		SchematronEvaluator schematronEvaluator = new DefaultSchematronEvaluator(schematron);
-		SchematronAssessment assessment = new SchematronAssessment(schematronEvaluator);
+		// Load the document to assess
+		DefaultXMLDocumentFactory documentFactory = new DefaultXMLDocumentFactory();
+		XMLDocument documentToAssess = documentFactory.load(new URL("classpath:templates/primary-swid.xml"));
 
+		// Create the assessment
+		Schematron schematron = new DefaultSchematronCompiler().newSchematron(new URL("classpath:schematron/swid-nistir-8060.sch"));
+		SchematronAssessment assessment = new SchematronAssessment(schematron, null);
+
+		// Establish the requirements
 		// TODO: Update this with a method to get an actual requirements manager instance
 		RequirementsManager requirementsManager = new StubRequirementsManager(Collections.emptySet());
-		BasicAssessmentExecutor executor = new BasicAssessmentExecutor(requirementsManager, Collections.singletonList(assessment));
 
-		XMLDocument doc = new JDOMDocument(new URL("classpath:templates/primary-swid.xml"));
-		AssessmentResults validationResult = executor.execute(doc);
+		// Perform the assessment
+		BasicAssessmentExecutor executor = new BasicAssessmentExecutor(requirementsManager, Collections.singletonList(assessment));
+		AssessmentResults validationResult = executor.execute(documentToAssess);
 
 		Collection<BaseRequirementResult> results = validationResult.getBaseRequirementResults();
 		for (BaseRequirementResult reqResult : results) {
