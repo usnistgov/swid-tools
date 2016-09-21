87a155abbc6d2a67ea9a446ab3a41ba23404061a
david-waltermire-nist <david.waltermire@nist.gov>
2016-09-21 03:12:16 +0000

    Added support for checking licenses using a Maven license plugin. Added support for looking up a TagType by name.

--- src/main/java/gov/nist/decima/swid/TagType.java
+++ src/main/java/gov/nist/decima/swid/TagType.java
@@ -1,10 +1,25 @@
 package gov.nist.decima.swid;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum TagType {
 	CORPUS("corpus"),
 	PRIMARY("primary"),
 	PATCH("patch"),
 	SUPPLEMENTAL("supplemental");
+
+	private static final Map<String, TagType> lookupMap = new HashMap<>();
+
+	static {
+		for (TagType type : TagType.values()) {
+			lookupMap.put(type.getName(), type);
+		}
+	}
+
+	public static TagType lookup(String value) {
+		return lookupMap.get(value);
+	}
 
 	private final String name;
 
