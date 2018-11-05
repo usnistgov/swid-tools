f257b6878e4370cfa25c9a2480b47f3773a88bc7
David Waltermire <david.waltermire@nist.gov>
2018-11-05 17:03:44 -0500

    Fixed formatting issue.

--- swidval/src/test/java/gov/nist/decima/swid/SWIDTagCharacteristicsTest.java
+++ swidval/src/test/java/gov/nist/decima/swid/SWIDTagCharacteristicsTest.java
@@ -20,6 +20,7 @@
  * PROPERTY OR OTHERWISE, AND WHETHER OR NOT LOSS WAS SUSTAINED FROM, OR AROSE OUT
  * OF THE RESULTS OF, OR USE OF, THE SOFTWARE OR SERVICES PROVIDED HEREUNDER.
  */
+
 package gov.nist.decima.swid;
 
 import gov.nist.decima.core.document.DocumentException;
